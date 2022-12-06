08d916c7235f0a9d47f705f343aaf27e86932e03
Cristian Larrauri <cristianlarrauri1@gmail.com>
2022-12-06 12:41:14 -0300

    Ruta get producto id

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,14 +1,11 @@
 const { Router } = require("express");
-const insumos = require("./insumos");
 
 const router = Router();
 
-
-router.use("/insumos", insumos);
-
+const insumos = require("./insumos");
 const productos = require("./productos");
 
+router.use("/insumos", insumos);
 router.use("/productos", productos);
-
 
 module.exports = router;
