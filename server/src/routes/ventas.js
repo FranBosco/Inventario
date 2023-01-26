3f187a201a44e9e0b7687377cec2ea8d35c47a75
Cristian Larrauri <cristianlarrauri1@gmail.com>
2023-01-26 16:55:44 -0300

    probando ruta ventas

--- server/src/routes/ventas.js
+++ server/src/routes/ventas.js
@@ -5,18 +5,22 @@ const router = Router();
 
 //.........................................................................................//
 // PUT /ventas
-router.put("/:id", async (req, res) => {
+router.put("/", async (req, res) => {
   try {
-    let { id } = req.params;
     let edit = req.body;
-    let prod = await Productos.findByPk(id);
 
-    await prod.update({
-      ...prod,
-      stock: Number(prod.stock) - Number(edit.stock),
+    edit.map(async (element) => {
+      if (element.stock) {
+        let prod = await Productos.findByPk(element.id);
+
+        await prod.update({
+          ...prod,
+          stock: Number(element.prod.stock) - Number(element.stock),
+        });
+
+        return res.status(200).send("Venta realizada con exito");
+      }
     });
-
-    return res.status(200).send("Venta realizada con exito");
   } catch (error) {
     console.log("ERROR EN RUTA PUT VENTAS");
   }
