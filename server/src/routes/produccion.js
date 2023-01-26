a7d3f1646db4f58cbe7884822213ff9679ccdd19
Cristian Larrauri <cristianlarrauri1@gmail.com>
2023-01-26 17:59:36 -0300

    bug ruta produccion

--- server/src/routes/produccion.js
+++ server/src/routes/produccion.js
@@ -3,28 +3,32 @@ const { Insumos, Productos } = require("../db");
 
 const router = Router();
 
-router.put("/:id", async (req, res) => {
+router.put("/", async (req, res) => {
   try {
-    let { id } = req.params;
     let edit = req.body;
     let prod = await Productos.findByPk(id);
 
-    await prod.update({
-      ...prod,
-      stock: Number(prod.stock) + Number(edit.stock),
+    edit.map(async (element) => {
+      if (element !== null) {
+        await prod.update({
+          ...prod,
+          stock: Number(prod.stock) + Number(element.stock),
+        });
+
+        await prod.defaultInput.forEach(async (el) => {
+          let aux = await Insumos.findAll({
+            where: { name: el.insumos },
+          });
+          let aux2 = aux[0];
+
+          await aux2.update({
+            ...aux2,
+            stock: Number(aux2.stock) - Number(el.cantidad) * element.stock,
+          });
+        });
+      }
     });
 
-    await prod.defaultInput.forEach(async (element) => {
-      let aux = await Insumos.findAll({
-        where: { name: element.insumos },
-      });
-      let aux2 = aux[0];
-
-      await aux2.update({
-        ...aux2,
-        stock: Number(aux2.stock) - Number(element.cantidad) * edit.stock,
-      });
-    });
     return res.status(200).send("Produccion agregada con exito");
   } catch (error) {
     console.log("ERROR EN RUTA PUT PRODUCCION", error);
